76a2c00ef26ecbe1b6d7027cfe62eb20fd62fca0
kentcheung2000 <kentcheung2000@yahoo.com>
2019-10-02 14:37:58 -0700

    Change to class base for QuestionShowPage

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionDetails from './QuestionDetails';
 import AnswerList from './AnswerList';
 import questionData from './questionData';
 
 // Question Show Component
-function QuestionShowPage(props) {
-    return (
-        <div>
-            <h1>Question Number {props.number}</h1>
-            <QuestionDetails
-                title={questionData[0].title}
-                body={questionData[0].body}
-                view_count={questionData[0].view_count}
-                author={questionData[0].author.full_name}
-                created_at={new Date(questionData[0].created_at)}
-            />
+class QuestionShowPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Question Number {this.props.number}</h1>
+                <QuestionDetails
+                    title={questionData[0].title}
+                    body={questionData[0].body}
+                    view_count={questionData[0].view_count}
+                    author={questionData[0].author.full_name}
+                    created_at={new Date(questionData[0].created_at)}
+                />
 
-            <AnswerList answers={questionData[0].answers} />
+                <AnswerList answers={questionData[0].answers} />
 
-            {/* <AnswerDetails body="Red." author={{ full_name: "Michael Owen" }} created_at={new Date()} /> */}
-        </div>
-    );
+                {/* <AnswerDetails body="Red." author={{ full_name: "Michael Owen" }} created_at={new Date()} /> */}
+            </div>
+        );
+    }
 }
+
 export default QuestionShowPage;
