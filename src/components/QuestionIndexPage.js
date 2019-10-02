5ceac462d052240b7266ce9dc0fe8a4fefd4d491
kentcheung2000 <kentcheung2000@yahoo.com>
2019-10-02 14:40:43 -0700

    Change to class base for QuestionIndexPage

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import questionData from './questionData';
 import '../styles/QuestionIndexPage.css';
 
-function QuestionIndexPage(props) {
-    return (
-        <main className="QuestionIndexPage">
-            <h1>Questions</h1>
-            <ul>
-                {questionData.map((question, index) => (
-                    <li key={question.id}>
-                        <p>
-                            {index} -
-							{question.title}
-                            <br />
-                            <small>Seen {question.view_count} time(s)</small>
-                            <small>Created at: {new Date(question.created_at).toLocaleDateString()}</small>
-                        </p>
-                        <p>{question.body}</p>
-                    </li>
-                ))}
-            </ul>
-        </main>
-    );
+class QuestionIndexPage extends Component {
+    render() {
+        return (
+            <main className="QuestionIndexPage">
+                <h1>Questions</h1>
+                <ul>
+                    {questionData.map((question, index) => (
+                        <li key={index}>
+                            <p>
+                                {question.title}
+                                <br />
+                                <small>Seen {question.view_count} time(s)</small>
+                                <small>Created at: {new Date(question.created_at).toLocaleDateString()}</small>
+                            </p>
+                            <p>{question.body}</p>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+        );
+    }
 }
 
 export default QuestionIndexPage;
